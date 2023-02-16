0785129eafdd1945234002136edce3ff14a45ecf
Ilya Bondarchuk <cod3provider@gmail.com>
2023-02-16 19:13:23 +0200

    small fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 import Container from './Container/Container';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
